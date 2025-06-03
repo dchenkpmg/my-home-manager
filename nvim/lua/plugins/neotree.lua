f62189d6712a1ab584fb8e748bc69c1ca4419c0e
dchenkpmg <dylanchen1@kpmg.co.nz>
2025-06-03 17:27:53 +1200

    chore: disable neotree close on last window
    doesn't play nice with restore session

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -8,7 +8,7 @@ return {
     "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
-  opts = {
-    close_if_last_window = true,
-  },
+  -- opts = {
+  --   close_if_last_window = true,
+  -- },
 }
