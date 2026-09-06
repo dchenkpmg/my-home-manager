51cbff9541e9c1af9f2bcee8e332c15af1dc1bee
mode-dch <128442751+mode-dch@users.noreply.github.com>
2026-09-06 18:38:46 +1200

    style: tidy option comments

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -13,40 +13,40 @@ opt.sidescrolloff = 8 -- Keep 8 columns left/right of cursor
 -- Indentation
 opt.tabstop = 2 -- Tab width
 opt.shiftwidth = 2 -- Indent width
-opt.softtabstop = 2 -- Soft tab stop NOT IN LAZYVIM
+opt.softtabstop = 2 -- Soft tab stop
 opt.expandtab = true -- Use spaces instead of tabs
 opt.smartindent = true -- Smart auto-indenting
 
 -- Search settings
 opt.ignorecase = true -- Case insensitive search
 opt.smartcase = true -- Case sensitive if uppercase in search
-opt.hlsearch = false -- Don't highlight search results NOT IN LAZYVIM
+opt.hlsearch = false -- Don't highlight search results
 
 -- Visual settings
 opt.termguicolors = true -- Enable 24-bit colors
 opt.signcolumn = "yes" -- Always show sign column
-opt.showmatch = true -- Highlight matching brackets NOT IN LAZYVIM
-opt.matchtime = 2 -- How long to show matching bracket NOT IN LAZYVIM
+opt.showmatch = true -- Highlight matching brackets
+opt.matchtime = 2 -- How long to show matching bracket
 opt.showmode = false -- Don't show mode in command line
 opt.pumheight = 10 -- Popup menu height
 opt.pumblend = 10 -- Popup menu transparency
 opt.completeopt = "menu,menuone,noselect"
 opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
 opt.confirm = true -- Confirm to save changes before exiting modified buffer
-opt.synmaxcol = 300 -- Syntax highlighting limit NOT IN LAZYVIM
+opt.synmaxcol = 300 -- Syntax highlighting limit
 opt.ruler = false -- Disable the default ruler
 opt.virtualedit = "block" -- Allow cursor to move where there is no text in visual block mode
 opt.winminwidth = 5 -- Minimum window width
 
 -- File handling
-opt.writebackup = false -- Don't create backup before writing NOT IN LAZYVIM
-opt.swapfile = false -- Don't create swap files NOT IN LAZYVIM
+opt.writebackup = false -- Don't create backup before writing
+opt.swapfile = false -- Don't create swap files
 opt.undofile = true -- Persistent undo
 opt.undolevels = 10000
-opt.undodir = vim.fn.expand("~/.vim/undodir") -- Undo directory NOT IN LAZYVIM
+opt.undodir = vim.fn.expand("~/.vim/undodir") -- Undo directory
 opt.updatetime = 200 -- Faster completion
 opt.timeoutlen = vim.g.vscode and 1000 or 300 -- Lower than default (1000) to quickly trigger which-key
-opt.ttimeoutlen = 0 -- Key code timeout NOT IN LAZYVIM
+opt.ttimeoutlen = 0 -- Key code timeout
 opt.autowrite = true -- Auto save
 opt.sessionoptions = {
 	"blank",
@@ -62,15 +62,15 @@ opt.sessionoptions = {
 }
 
 -- Behavior settings
-opt.iskeyword:append("-") -- Treat dash as part of word NOT IN LAZYVIM
-opt.path:append("**") -- include subdirectories in search NOT IN LAZYVIM
-opt.selection = "exclusive" -- Selection behavior NOT IN LAZYVIM
+opt.iskeyword:append("-") -- Treat dash as part of word
+opt.path:append("**") -- include subdirectories in search
+opt.selection = "exclusive" -- Selection behavior
 opt.mouse = "a" -- Enable mouse support
 opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus" -- Sync with system clipboard
 
 -- Folding settings
 opt.smoothscroll = true
-vim.wo.foldmethod = "expr" -- NOT IN LAZYVIM
+vim.wo.foldmethod = "expr"
 opt.foldlevel = 99 -- Start with all folds open
 opt.foldtext = ""
 opt.formatoptions = "jcroqlnt" -- tcqj
@@ -83,17 +83,17 @@ opt.splitkeep = "screen"
 
 -- Command-line completion
 opt.wildmode = "longest:full,full"
-opt.wildignore:append({ "*.o", "*.obj", "*.pyc", "*.class", "*.jar" }) -- NOT IN LAZYVIM
+opt.wildignore:append({ "*.o", "*.obj", "*.pyc", "*.class", "*.jar" })
 
 -- Better diff options
-opt.diffopt:append("linematch:60") -- NOT IN LAZYVIM
+opt.diffopt:append("linematch:60")
 
 -- Performance improvements
-opt.redrawtime = 10000 -- NOT IN LAZYVIM
-opt.maxmempattern = 20000 -- NOT IN LAZYVIM
+opt.redrawtime = 10000
+opt.maxmempattern = 20000
 
 -- Create undo directory if it doesn't exist
-local undodir = vim.fn.expand("~/.vim/undodir") -- NOT IN LAZYVIM
+local undodir = vim.fn.expand("~/.vim/undodir")
 if vim.fn.isdirectory(undodir) == 0 then
 	vim.fn.mkdir(undodir, "p")
 end
@@ -116,7 +116,7 @@ opt.shortmess:append({ W = true, I = true, c = true, C = true })
 
 vim.g.markdown_recommended_style = 0
 
-vim.filetype.add({ -- NOT IN LAZYVIM
+vim.filetype.add({
 	extension = {
 		env = "dotenv",
 	},
